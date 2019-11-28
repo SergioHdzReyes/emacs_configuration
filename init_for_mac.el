7dc74a8e942c235672db7bc106477e21ebdf6021
Sergio Hernandez Reyes <sergioreyes_90290@hotmail.com>
2019-11-28 11:52:27 -0600

    Php autocomplete added, flycheck, wakatime, treemacs, web-mode

--- init_for_mac.el
+++ init_for_mac.el
@@ -1,17 +1,54 @@
-;; AÑADIDOS POR MI START_
-(setq default-directory (concat (getenv "HOME") "/Proyectos/Enviaflores/"))
-(set-terminal-coding-system 'utf-8)
-(set-keyboard-coding-system 'utf-8)
-(prefer-coding-system 'utf-8)
+;;; init.el --- Init
 
-;;(global-set-key (kbd "s-1") "$this->APIV2->debug = true;")
+;;; Commentary:
+;;
+;; Init
+;;
+
+(require 'package)
+(let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
+                    (not (gnutls-available-p))))
+       (proto (if no-ssl "http" "https")))
+  (when no-ssl
+    (warn "\
+Your version of Emacs does not support SSL connections,
+which is unsafe because it allows man-in-the-middle attacks.
+There are two things you can do about this warning:
+1. Install an Emacs version that does support SSL and be safe.
+2. Remove this warning from your init file so you won't see it again."))
+  ;; Comment/uncomment these two lines to enable/disable MELPA and MELPA Stable as desired
+  (add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t)
+  ;;(add-to-list 'package-archives (cons "melpa-stable" (concat proto "://stable.melpa.org/packages/")) t)
+  (when (< emacs-major-version 24)
+    ;; For important compatibility libraries like cl-lib
+    (add-to-list 'package-archives (cons "gnu" (concat proto "://elpa.gnu.org/packages/")))))
+(package-initialize)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes '(dracula))
+ '(custom-safe-themes
+   '("947190b4f17f78c39b0ab1ea95b1e6097cc9202d55c73a702395fc817f899393" default))
+ '(global-flycheck-mode t)
+ '(global-wakatime-mode t)
+ '(package-selected-packages
+   '(treemacs restclient flycheck-clangcheck flycheck-clang-analyzer auto-complete-clang auto-complete-clang-async auto-complete-c-headers ac-clang dumb-jump web-mode tern org magit google-translate dracula-theme dash-functional company-php ac-php))
+ '(safe-local-variable-values '((flycheck-gcc-language-standard . c99)))
+ '(wakatime-python-bin nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
 
 
-(add-to-list 'auto-mode-alist '("\\.ctp$" . php-mode))
-(global-set-key (kbd "s-º") "\\")
-(global-set-key (kbd "s-1") "|")
-(global-set-key (kbd "s-2") "@")
-(global-set-key (kbd "s-3") "#")
+;; START
+(setq wakatime-api-key "")
+(setq wakatime-cli-path "/Library/Frameworks/Python.framework/Versions/3.7/bin/wakatime")
+;;(global-wakatime-mode)
 
 ;; Funcion para ir a primer caracter de linea o a bloque de texto
 (defun xah-beginning-of-line-or-block ()
@@ -29,101 +66,80 @@
         (when (eq $p (point))
           (beginning-of-line))))))
 
-;; Funcion para ir a final de linea o bloque de texto
-(defun xah-end-of-line-or-block ()
-  (interactive)
-  (if (or (equal (point) (line-end-position))
-          (equal last-command this-command ))
-      (progn
-        (re-search-forward "\n[\t\n ]*\n+" nil "NOERROR" ))
-    (end-of-line)))
-
 (global-set-key (kbd "C-a") 'xah-beginning-of-line-or-block)
-(global-set-key (kbd "C-e") 'xah-end-of-line-or-block)
 
+(require 'php-mode)
 
-;; Funciones para ir a inicio o fin de corchetes/llaves/parentesis etc
-(defvar xah-brackets nil "string of left/right brackets pairs.")
-(setq xah-brackets "()[]{}<>（）［］｛｝⦅⦆〚〛⦃⦄“”‘’‹›«»「」〈〉《》【】〔〕⦗⦘『』〖〗〘〙｢｣⟦⟧⟨⟩⟪⟫⟮⟯⟬⟭⌈⌉⌊⌋⦇⦈⦉⦊❛❜❝❞❨❩❪❫❴❵❬❭❮❯❰❱❲❳〈〉⦑⦒⧼⧽﹙﹚﹛﹜﹝﹞⁽⁾₍₎⦋⦌⦍⦎⦏⦐⁅⁆⸢⸣⸤⸥⟅⟆⦓⦔⦕⦖⸦⸧⸨⸩｟｠⧘⧙⧚⧛⸜⸝⸌⸍⸂⸃⸄⸅⸉⸊᚛᚜༺༻༼༽⏜⏝⎴⎵⏞⏟⏠⏡﹁﹂﹃﹄︹︺︻︼︗︘︿﹀︽︾﹇﹈︷︸")
-
-(defvar xah-left-brackets '("(" "{" "[" "<" "〔" "【" "〖" "〈" "《" "「" "『" "“" "‘" "‹" "«" )
-  "List of left bracket chars.")
-(progn
-;; make xah-left-brackets based on xah-brackets
-  (setq xah-left-brackets '())
-  (dotimes ($x (- (length xah-brackets) 1))
-    (when (= (% $x 2) 0)
-      (push (char-to-string (elt xah-brackets $x))
-            xah-left-brackets)))
-  (setq xah-left-brackets (reverse xah-left-brackets)))
-
-(defvar xah-right-brackets '(")" "]" "}" ">" "〕" "】" "〗" "〉" "》" "」" "』" "”" "’" "›" "»")
-  "list of right bracket chars.")
-(progn
-  (setq xah-right-brackets '())
-  (dotimes ($x (- (length xah-brackets) 1))
-    (when (= (% $x 2) 1)
-      (push (char-to-string (elt xah-brackets $x))
-            xah-right-brackets)))
-  (setq xah-right-brackets (reverse xah-right-brackets)))
-
-(defun xah-backward-left-bracket ()
-  "Move cursor to the previous occurrence of left bracket.
-The list of brackets to jump to is defined by `xah-left-brackets'.
-URL `http://ergoemacs.org/emacs/emacs_navigating_keys_for_brackets.html'
-Version 2015-10-01"
-  (interactive)
-  (re-search-backward (regexp-opt xah-left-brackets) nil t))
-
-(defun xah-forward-right-bracket ()
-  "Move cursor to the next occurrence of right bracket.
-The list of brackets to jump to is defined by `xah-right-brackets'.
-URL `http://ergoemacs.org/emacs/emacs_navigating_keys_for_brackets.html'
-Version 2015-10-01"
-  (interactive)
-  (re-search-forward (regexp-opt xah-right-brackets) nil t))
-
-(global-set-key (kbd "C-c l") 'xah-backward-left-bracket)
-(global-set-key (kbd "C-c r") 'xah-forward-right-bracket)
-
-
-(defun xah-goto-matching-bracket ()
-  "Move cursor to the matching bracket.
-If cursor is not on a bracket, call `backward-up-list'.
-The list of brackets to jump to is defined by `xah-left-brackets' and `xah-right-brackets'.
-URL `http://ergoemacs.org/emacs/emacs_navigating_keys_for_brackets.html'
-Version 2016-11-22"
-  (interactive)
-  (if (nth 3 (syntax-ppss))
-      (backward-up-list 1 'ESCAPE-STRINGS 'NO-SYNTAX-CROSSING)
-    (cond
-     ((eq (char-after) ?\") (forward-sexp))
-     ((eq (char-before) ?\") (backward-sexp ))
-     ((looking-at (regexp-opt xah-left-brackets))
-      (forward-sexp))
-     ((looking-back (regexp-opt xah-right-brackets) (max (- (point) 1) 1))
-      (backward-sexp))
-     (t (backward-up-list 1 'ESCAPE-STRINGS 'NO-SYNTAX-CROSSING)))))
-
-;; Configuracion para google translate
-(customize-set-variable 'google-translate-default-source-language "en")
-(customize-set-variable 'google-translate-default-target-language "es")
-
-(global-set-key (kbd "C-c C-t") 'google-translate-query-translate)
-(global-set-key (kbd "C-c C-s") 'google-translate-smooth-translate)
-
-
-;; PHP configuration
 (add-hook 'php-mode-hook
           '(lambda ()
+             ;; Enable auto-complete-mode
              (auto-complete-mode t)
+
              (require 'ac-php)
              (setq ac-sources '(ac-source-php))
+
+             ;; As an example (optional)
              (yas-global-mode 1)
 
-             (define-key php-mode-map (kbd "C-]")
+             ;; Enable ElDoc support (optional)
+             (ac-php-core-eldoc-setup)
+
+             ;; Jump to definition (optional)
+             (define-key php-mode-map (kbd "M-]")
                'ac-php-find-symbol-at-point)
-             (define-key php-mode-map (kbd "C-t")
+
+             ;; Return back (optional)
+             (define-key php-mode-map (kbd "M-[")
                'ac-php-location-stack-back)))
 
-;; AÑADIDOS POR MI END_
+(require 'php-mode)
+
+(add-hook 'php-mode-hook
+          '(lambda ()
+             ;; Enable company-mode
+             (company-mode t)
+             (require 'company-php)
+
+             ;; Enable ElDoc support (optional)
+             (ac-php-core-eldoc-setup)
+
+             (set (make-local-variable 'company-backends)
+                  '((company-ac-php-backend company-dabbrev-code)
+                    company-capf company-files))
+
+             ;; Jump to definition (optional)
+             (define-key php-mode-map (kbd "M-]")
+               'ac-php-find-symbol-at-point)
+
+             ;; Return back (optional)
+             (define-key php-mode-map (kbd "M-[")
+               'ac-php-location-stack-back)))
+
+(add-to-list 'auto-mode-alist '("\\.ctp$" . php-mode))
+(global-set-key (kbd "s-º") "\\")
+(global-set-key (kbd "s-1") "|")
+(global-set-key (kbd "s-2") "@")
+(global-set-key (kbd "s-3") "#")
+(global-set-key (kbd "M-ç") "}")
+(global-set-key (kbd "M-+") "]")
+(global-set-key (kbd "M-ñ") "~")
+(global-set-key (kbd "C-x C-b") 'buffer-menu)
+(global-set-key (kbd "C-x C-k") 'kill-buffer-and-window)
+
+(setq default-directory (concat (getenv "HOME") "/Proyectos/Enviaflores/"))
+;; (set-terminal-coding-system 'utf-8)
+;; (set-keyboard-coding-system 'utf-8)
+;; (prefer-coding-system 'utf-8)
+
+(global-set-key (kbd "C-c m") 'comment-or-uncomment-region)
+(global-set-key (kbd "C-x C-b") 'ibuffer)
+
+;; Configuracion para GOOGLE TRANSLATE
+(customize-set-variable 'google-translate-default-source-language "en")
+(customize-set-variable 'google-translate-default-target-language "es")
+(global-set-key (kbd "C-c C-t") 'google-translate-query-translate)
+(global-set-key (kbd "C-c C-s") 'google-translate-smooth-translate)
+;; END
+
+(provide 'init)
+;;; init.el ends here
