75fdcf64cfbcba90cc023c0a938ca2f90ea8a8ee
Sergio Hernández Reyes <sergio.hdzreyes@gmail.com>
2021-03-30 17:29:47 -0600

    Función y comando para añadir transparencia a Emacs

--- package-configurations/config-general.el
+++ package-configurations/config-general.el
@@ -18,5 +18,16 @@
 (show-paren-mode 1)
 (scroll-bar-mode -1)
 
+(defun set-frame-alpha (arg &optional active)
+  (interactive "nEnter alpha value (1-100): \np")
+  (let* ((elt (assoc 'alpha default-frame-alist))
+         (old (frame-parameter nil 'alpha))
+         (new (cond ((atom old)     `(,arg ,arg))
+                    ((eql 1 active) `(,arg ,(cadr old)))
+                    (t              `(,(car old) ,arg)))))
+    (if elt (setcdr elt new) (push `(alpha ,@new) default-frame-alist))
+    (set-frame-parameter nil 'alpha new)))
+(global-set-key (kbd "C-c t") 'set-frame-alpha)
+
 (provide 'config-general)
 ;;; config-general.el ends here
