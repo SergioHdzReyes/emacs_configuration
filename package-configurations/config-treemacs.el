74d992c7c9ede90473f89b33a804965c8005f6b4
Sergio Hernández Reyes <sergio.hdzreyes@toshibagcs.com>
2023-09-21 10:12:08 -0600

    Soporte a git y seguimiento de proyecto actual.

--- package-configurations/config-treemacs.el
+++ package-configurations/config-treemacs.el
@@ -1,7 +1,16 @@
-;;; TREEMACS
+;;; treemacs --- Summary
+;;; Commentary:
+;;; Code:
 
 (use-package treemacs
   :init
-  (global-set-key (kbd "M-g M-r") 'treemacs))
+  (global-set-key (kbd "M-g M-r") 'treemacs)
+  (treemacs-add-and-display-current-project-exclusively)
+  (setq treemacs-project-follow-mode t)
+  (setq treemacs-indent-guide-mode t)
+  ;;;(setq treemacs-follow-mode t)
+  (setq treemacs-git-mode t)
+  )
 
 (provide 'config-treemacs)
+;;; config-treemacs.el ends here
